35aeb0888a8a9b4b6407ad79556b051cf7863fe0
Kaleb Bordner <kaleb.bordner@gmail.com>
2025-10-02 16:24:31 -0700

    running build_pyoptsparse with uv

--- update_submodules.sh
+++ update_submodules.sh
@@ -6,5 +6,7 @@ git submodule update --init --recursive
 # this updates the submodules to the latest commit in their respective repositories
 git submodule sync
 
+./build_pyoptsparse/build_pyoptsparse.py -d --pip-cmd='uv pip'
+
 # this pulls the latest changes from the remote repositories for each submodule
 uv sync
